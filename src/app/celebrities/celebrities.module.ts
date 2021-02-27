6254cb5f2701e48409c73cd93482fe4058040c45
Daniel Lopez <dlopezdelgado@gmail.com>
2021-02-27 18:31:49 -0500

    Angular Material

--- src/app/celebrities/celebrities.module.ts
+++ src/app/celebrities/celebrities.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CelebritiesRoutingModule } from './celebrities-routing.module';
 import { CelebritiesListComponent } from './components/celebrities-list/celebrities-list.component';
 import { CelebrityCardComponent } from './components/celebrity-card/celebrity-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [CelebritiesListComponent, CelebrityCardComponent],
   imports: [
     CommonModule,
-    CelebritiesRoutingModule
+    CelebritiesRoutingModule,
+    SharedModule
   ]
 })
 export class CelebritiesModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './components/ui/error/error.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoaderComponent } from './components/ui/loader/loader.component';
 
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
-  declarations: [ErrorComponent]
+  declarations: [
+    ErrorComponent, 
+    LoaderComponent
+  ],
+  exports: [
+    LoaderComponent
+  ]
 })
 export class SharedModule { }
